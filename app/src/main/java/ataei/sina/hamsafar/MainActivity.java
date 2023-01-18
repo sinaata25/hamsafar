ca2ed807bbcf4d64084420567e2e61e4b8a69a49
ACER <reza0baghernataj@gmail.com>
2023-01-19 02:01:50 +0330

    Reza: main navigation completde

--- app/src/main/java/ataei/sina/hamsafar/MainActivity.java
+++ app/src/main/java/ataei/sina/hamsafar/MainActivity.java
@@ -1,14 +1,99 @@
 package ataei.sina.hamsafar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ImageView;
+
+import ataei.sina.hamsafar.adapters.main_page_adapter;
+import ataei.sina.hamsafar.fragments.DiscoveryFragment;
+import ataei.sina.hamsafar.fragments.HistoryFragment;
+import ataei.sina.hamsafar.fragments.HomeFragment;
+import ataei.sina.hamsafar.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager2 main_viewpager;
+    ImageView Home_bot , Discovery_bot , History_bot , Profile_bot;
+    main_page_adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setUpViews();
+        adapter = new main_page_adapter(getSupportFragmentManager(), getLifecycle() , getApplicationContext());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new DiscoveryFragment());
+        adapter.addFragment(new HistoryFragment());
+        adapter.addFragment(new ProfileFragment());
+
+        main_viewpager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        main_viewpager.setCurrentItem(0);
+        main_viewpager.setAdapter(adapter);
+        main_viewpager.setUserInputEnabled(false);
+
+    }
+
+    private void unselected(int x) {
+        if(x == 0){
+            Home_bot.setImageResource(R.mipmap.home);
+            return;
+        }
+        if(x == 1){
+            Discovery_bot.setImageResource(R.mipmap.discover);
+            return;
+        }
+        if(x == 2){
+            History_bot.setImageResource(R.mipmap.history);
+            return;
+        }
+        if(x == 3){
+            Profile_bot.setImageResource(R.mipmap.profile);
+            return;
+        }
+    }
+
+
+    private void setUpViews() {
+        main_viewpager = findViewById(R.id.main_viewpager);
+        Home_bot = findViewById(R.id.Home_bot);
+        Discovery_bot = findViewById(R.id.Discovery_bot);
+        History_bot = findViewById(R.id.History_bot);
+        Profile_bot = findViewById(R.id.Profile_bot);
+    }
+
+
+    public void Home_clicked(View view) {
+
+        unselected(main_viewpager.getCurrentItem());
+        Home_bot.setImageResource(R.mipmap.home_selected);
+        main_viewpager.setCurrentItem(0);
+
+
+    }
+    public void Discovery_clicked(View view) {
+
+        unselected(main_viewpager.getCurrentItem());
+        Discovery_bot.setImageResource(R.mipmap.dicover_selected);
+        main_viewpager.setCurrentItem(1);
+
+    }
+    public void History_clicked(View view) {
+
+        unselected(main_viewpager.getCurrentItem());
+        History_bot.setImageResource(R.mipmap.history_selected);
+        main_viewpager.setCurrentItem(2);
+
+    }
+    public void Profile_clicked(View view) {
+
+        unselected(main_viewpager.getCurrentItem());
+        Profile_bot.setImageResource(R.mipmap.profile_selected);
+        main_viewpager.setCurrentItem(3);
+
     }
 }
---
+++ app/src/main/java/ataei/sina/hamsafar/adapters/main_page_adapter.java
@@ -0,0 +1,36 @@
+package ataei.sina.hamsafar.adapters;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main_page_adapter extends FragmentStateAdapter {
+
+    List<Fragment>fragments;
+    Context ctx;
+
+    public main_page_adapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle , Context context) {
+        super(fragmentManager , lifecycle);
+        ctx = context;
+        fragments = new ArrayList<>();
+    }
+
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+    public void addFragment(Fragment fragment) {
+        fragments.add(fragment);
+    }
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
