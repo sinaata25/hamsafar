f81c33c270b1390d12293916dc930edaa9ac229f
sinaata25 <sina.ata25@gmail.com>
2023-01-19 12:45:51 -0800

    sina: name loaded in home and profile

---
+++ app/src/main/java/ataei/sina/hamsafar/AskToTrip.java
@@ -0,0 +1,14 @@
+package ataei.sina.hamsafar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AskToTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ask_to_trip);
+    }
+}
