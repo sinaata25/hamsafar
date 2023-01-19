8becb9568ea533bfa72cde492b95e7b098aef868
sinaata25 <sina.ata25@gmail.com>
2023-01-19 09:01:45 -0800

    sina: login ad logup pages and navigations complited 100%

--- app/src/main/java/ataei/sina/hamsafar/fragments/HomeFragment.java
+++ app/src/main/java/ataei/sina/hamsafar/fragments/HomeFragment.java
@@ -1,9 +1,12 @@
 package ataei.sina.hamsafar.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,23 @@ import ataei.sina.hamsafar.R;
 
 public class HomeFragment extends Fragment {
     @Nullable
+    TextView textView_welcome;
     View view;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.home_page,container,false);
+        setupViews();
+        sets();
         return view;
+    }
+
+    private void sets() {
+        SharedPreferences sharedPref = getContext().getSharedPreferences("shared", Context.MODE_PRIVATE);
+        String name=sharedPref.getString("name","");
+        textView_welcome.setText("خوش اومدی "+name);
+    }
+
+    private void setupViews() {
+        textView_welcome=view.findViewById(R.id.textView5_home);
     }
 
 }
--- app/src/main/java/ataei/sina/hamsafar/fragments/ProfileFragment.java
+++ app/src/main/java/ataei/sina/hamsafar/fragments/ProfileFragment.java
@@ -1,9 +1,12 @@
 package ataei.sina.hamsafar.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +17,26 @@ import ataei.sina.hamsafar.R;
 public class ProfileFragment extends Fragment {
     @Nullable
     View view;
+    TextView name,username;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.profile_page,container,false);
+        setUpViews();
+        setUps();
         return view;
+    }
+
+    private void setUps() {
+        SharedPreferences sharedPref = getContext().getSharedPreferences("shared", Context.MODE_PRIVATE);
+        String _name=sharedPref.getString("name","");
+        String _family=sharedPref.getString("family","");
+        String _username=sharedPref.getString("username","");
+        name.setText(_name+" "+_family);
+        username.setText(_username);
+    }
+
+    private void setUpViews() {
+        name=view.findViewById(R.id.profile_name);
+        username=view.findViewById(R.id.id_name);
     }
 
 }
