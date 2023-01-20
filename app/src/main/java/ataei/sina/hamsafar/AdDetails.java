1740092c2ebae42711f22696bdce990d8c9977f0
ACER <reza0baghernataj@gmail.com>
2023-01-20 21:29:05 +0330

    Reza: ads detail az tarafe home page done

--- app/src/main/java/ataei/sina/hamsafar/AdDetails.java
+++ app/src/main/java/ataei/sina/hamsafar/AdDetails.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ataei.sina.hamsafar.model.Advertisment;
 
 public class AdDetails extends AppCompatActivity {
         TextView detail,origin,destination,date,time,car,username;
+
+        ImageView back;
     Advertisment advertisment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +32,39 @@ public class AdDetails extends AppCompatActivity {
 
     private void sets() {
         detail.setText(advertisment.getDescription());
+        origin.setText(advertisment.getOrigin());
+        destination.setText(advertisment.getDestination());
+        time.setText(advertisment.getTime());
+        date.setText(advertisment.getDate());
+        username.setText(advertisment.getName());
 
     }
 
     private void setUpViews() {
         detail=findViewById(R.id.detail_description);
+        origin=findViewById(R.id.detail_text_origin);
+        destination=findViewById(R.id.detail_text_destination);
+        time=findViewById(R.id.detail_text_time);
+        date=findViewById(R.id.detail_text_date);
+        username=findViewById(R.id.detail_driver_name);
+        back=findViewById(R.id.detail_buttom_back);
     }
 
     private void handle() {
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
    public interface DataL{
         void onGetL(Advertisment advertisment);
     }
+
+
 
 
 }
